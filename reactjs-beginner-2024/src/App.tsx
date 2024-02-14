f8788484cfed1a87fc3213f7471e4818131b68d9
Andre Hochschulte <andre@hochschulte.com>
2024-02-15 00:00:07 +0100

    Implementierung TodoListe

--- reactjs-beginner-2024/src/App.tsx
+++ reactjs-beginner-2024/src/App.tsx
@@ -1,35 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { dummyData } from "./data/todos";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <main className="py-10 h-screen">
+      <h1 className="font-bold text-3xl text-center">Your Todos</h1>
+      <div className="max-w-lg mx-auto">
+        <div className="space-y-2">
+          {dummyData.map((todo) => (
+            <p key={todo.id} className="text-lg">
+              {todo.title}
+            </p>
+          ))}
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </main>
+  );
 }
 
-export default App
+export default App;
---
+++ reactjs-beginner-2024/src/data/todos.ts
@@ -0,0 +1,19 @@
+import { Todo } from "../types/todos";
+
+export const dummyData: Todo[] = [
+  {
+    id: 1,
+    title: "Like this video",
+    completed: false,
+  },
+  {
+    id: 2,
+    title: "Subcribe to Coding in Flow",
+    completed: false,
+  },
+  {
+    id: 3,
+    title: "Leave a nice comment",
+    completed: false,
+  },
+];
